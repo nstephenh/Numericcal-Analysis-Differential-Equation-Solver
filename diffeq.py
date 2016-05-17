b31d60d190f1fab8e5bcaa238d970f4c7e3206b3
Noah Stephen Haskell <nstephenh@gmail.com>
2016-05-17 12:29:00 -0400

    RK4

--- diffeq.py
+++ diffeq.py
@@ -14,6 +14,22 @@ def euler(function, iterations, initialx, initialy, interval=1, divisor=","):
     print(i, divisor, currx, divisor, curry)
     return(curry)
 
+def rk4(function, iterations, initialx, initialy, interval=1, divisor=","):
+    currx = initialx
+    curry = initialy
+    i = 0
+    h = interval/iterations
+    while (i < iterations):
+        print(i, divisor, currx, divisor, curry)
+        m1 = function(currx, curry)
+        m2 = function(currx+(h/2),curry+(h*m1/2))
+        m3 = function(currx+(h/2), curry+(h*m2/2))
+        m4 = function((currx+h), curry+(h*m3))
+        curry += (h/6)*(m1+2*m2+2*m3+m4)
+        currx += h
+        i += 1
+    print(i, divisor, currx, divisor, curry)
+    return(curry)
 
 def quadeuler(function, deriv,  iterations, initialx, initialy, interval=1, divisor=","):
     pastx = initialx
@@ -44,5 +60,6 @@ def difeq2(x, y):
 
 initx = 1
 print(euler(difeq1, 20, 1, abs(initx)))
-print(euler(difeq2, 20, 1, 1))
+#print(euler(difeq2, 20, 1, 1))
 print(quadeuler(difeq1, difderiv, 20, 1, 1))
+print(rk4(difeq1, 20, 1, abs(initx)))
